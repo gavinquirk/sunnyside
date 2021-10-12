c5a8e12bd5d6c421fcd264869f4fc7bc9f868b6d
Gavin Quirk <gavinquirk@gmail.com>
2021-10-12 16:02:35 -0700

    container, theme colors

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider } from 'styled-components';
+import { Container } from './components/styles/Container.styled';
+
+const theme = {
+  colors: {
+    primary: {
+      softRed: 'hsl(7, 99%, 70%)',
+      yellow: 'hsl(51, 100%, 49%)',
+      darkDesaturatedCyan: 'hsl(167, 40%, 24%)',
+      darkBlue: 'hsl(198, 62%, 26%)',
+      darkModerateCyan: 'hsl(168, 34%, 41%)',
+    },
+    Neutral: {
+      veryDarkDesaturatedBlue: 'hsl(212, 27%, 19%)',
+      veryDarkGrayishBlue: 'hsl(213, 9%, 39%)',
+      darkGrayishBlue: 'hsl(232, 10%, 55%)',
+      grayishBlue: 'hsl(210, 4%, 67%)',
+      white: 'hsl(0, 0%, 100%)',
+    },
+  },
+};
 
 function App() {
   return (
-    <div className='App'>
-      <header>
-        <h1>React App</h1>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <>
+        <Container>
+          <h1>We Are Creatives</h1>
+        </Container>
+      </>
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header>
+      <h1>We Are Creatives</h1>
+    </header>
+  );
+}
